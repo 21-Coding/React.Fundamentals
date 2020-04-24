376f85b486e8521b46337d401b548f34e9e94a34
DominiqueYoumans <ddyoumans@icloud.com>
2020-04-24 11:39:14 -0700

    add new constructors.

--- src/Components/Kegs/KegControl.js
+++ src/Components/Kegs/KegControl.js
@@ -3,4 +3,14 @@ import AddingKegs from './Components/Kegs/AddKegForm.js';
 import KegList from './Components/Kegs/KegList.js';
 
 
-class KegControl extends React.Component {}
+class KegControl extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            formVisibleOnPage: false
+        };
+    }
+
+    
+}
