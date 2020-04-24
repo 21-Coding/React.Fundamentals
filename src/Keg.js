b3b88dd71f4546e71e9dede92a89009ae7a4bb7c
DominiqueYoumans <ddyoumans@icloud.com>
2020-04-24 13:18:35 -0700

    add keg type css.

--- src/Keg.js
+++ src/Keg.js
@@ -2,6 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Keg(props) {
-
+    const kegTypes = {
+        height: '200px',
+        width: '200px',
+        border: '1px solid black',
+        borderRadius: '25px',
+        padding: '5%',
+    }
 
 }
