aa9dc17115d8d570098b663bdc0d8a312e5e877d
DominiqueYoumans <ddyoumans@icloud.com>
2020-04-24 14:21:19 -0700

    add function for adding kegs.

--- src/KegControl.js
+++ src/KegControl.js
@@ -9,8 +9,18 @@ class KegControl extends React.Component {
         super(props);
         this.state = {
             formVisibleOnPage: false
+            masterKegList: []
         };
     }
+
+    handleAddKegToList = (newKeg) => {
+        const newMasterKegList = this.state.masterKegList.concat(newKeg);
+        this.setState({
+            masterKegList: newMasterKegList,
+            formVisibleOnPage: false
+        })
+    }
+
 
     
 }
