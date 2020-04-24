c29c0b07e459161d0b9494f12ea73f3b4cf0fb93
DominiqueYoumans <ddyoumans@icloud.com>
2020-04-24 11:23:36 -0700

    add new routing to import statement.

--- src/Components/Kegs/KegControl.js
+++ src/Components/Kegs/KegControl.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import AddingKegs from './Components/Kegs/AddKegForm.js';
+import KegList from './Components/Kegs/KegList.js';
 
 
 class KegControl extends React.Component {}
