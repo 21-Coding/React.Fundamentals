8e3add12271b7865ff2520c8122f3f3fe25e693c
DominiqueYoumans <ddyoumans@icloud.com>
2020-04-25 13:20:15 -0700

    begin return statement.

--- src/ReusableForm.js
+++ src/ReusableForm.js
@@ -4,5 +4,17 @@ import PropTypes from 'prop-types';
 function ReusableForm(props) {
 
     const formStyles = {
-        width: '40v'
+        width: '40v',
+        margin: 'auto auto',
+        backgroundColor: 'transparent',
+        padding: '5%',
+        marginBottom: '2%'
     }
+
+    return (
+        <React.Fragment>
+            <div>
+                
+            </div>
+        </React.Fragment>
+    )
